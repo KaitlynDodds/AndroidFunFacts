393ed77a018f9fa3283a9e82375b6d211846a680
kadodds <kadodds@microsoft.com>
2017-06-11 13:46:41 -0700

    Finished random fact picker

--- app/src/main/java/com/treehouse/kadodds/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/treehouse/kadodds/funfacts/FunFactsActivity.java
@@ -6,11 +6,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class FunFactsActivity extends AppCompatActivity {
 
 //    declare our View variables
     private TextView factTextView;
     private Button showFactBtn;
+
+//    Facts Array
+    private String[] facts = {
+        "Ants stretch when they wake up in the morning.",
+        "Ostriches can run faster than horses.",
+        "Olympic gold medals are actually made mostly of silver.",
+        "You are born with 300 bones; by the time you are an adult you will have 206.",
+        "It takes about 8 minutes for light from the Sun to reach Earth.",
+        "Some bamboo plants can grow almost a meter in just one day.",
+        "The state of Florida is bigger than England.",
+        "Some penguins can leap 2-3 meters out of the water.",
+        "On average, it takes 66 days to form a new habit.",
+        "Mammoths still walked the earth when the Great Pyramid was being built."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +41,13 @@ public class FunFactsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // button was clicked, update factTextView with new fact
-                String fact = "Ostriches can run faster than horses.";
+                String fact = "";
+
+//                select random fact
+                Random randomGen = new Random();
+                int randNum = randomGen.nextInt(facts.length);
+                fact = facts[randNum];
+//                update screen w/ dynamic fact
                 factTextView.setText(fact);
             }
         };
